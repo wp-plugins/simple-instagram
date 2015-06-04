3fcc54ea5e53a707813e6d5c53c70a24f0b589fb
mr_speer <mr_speer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-04 14:51:01 +0000

    tagging version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/simple-instagram/tags/2.0@1174484 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/views/admin.php
+++ admin/views/admin.php
@@ -8,100 +8,51 @@
  * @package   Simple Instagram
  * @author    Aaron Speer <adspeer@gmail.com>
  * @license   GPL-2.0+
- * @copyright 2013 Your Name or Company Name
+ * @copyright 2013 Press75
  */
-?>
 
-<?php 
-  $options_check = get_option('si_options');
-  if(strlen($options_check['instagram_app_id']) > 0 && strlen($options_check['instagram_app_secret']) > 0){
-    $set = 1;
-  }else{
-    $set = 0;
-  }
-  
-  if(isset($options_check['instagram_oauth']) && strlen($options_check['instagram_oauth']) > 0){
-    $auth = 1;
-  }else{
-    $auth = 0;
-  }
-?>
+if ( isset( $_GET['token'] ) ) {
+    $token = sanitize_text_field( $_GET['token'] );
+    update_option( 'si_access_token', $_GET['token'] );
+}
 
-<?php if(isset($_GET['settings-updated'])){ update_option('si_oauth', ''); } ?>
+$token = get_option( 'si_access_token' ) ? get_option( 'si_access_token' ) : null;
+
+?>
 
 <div class="wrap">
-
-	<?php screen_icon(); ?>
-	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
-	<section id="step01">
-	  <div class="section_title">
-	    Step 01: Register Your Application with Instagram
-	  </div>
-	  <div class="section_content <?php if($set == 0){ echo "active"; } ?>">
-	    <ol>
-        <li>Visit <a href="http://instagram.com/developer/" rel="nofollow">http://instagram.com/developer/</a> and log in (click "login" in the upper-righthand corner)</li>
-        <li>Once you've logged in, you'll be taken to your Edit Profile page. Make any changes you'd like, and then click on the API link in the footer of the page. This will return you to the Developer homepage.</li>
-        <li>Click the "Register Your Application" button.</li>
-        <li>Click "Register a New Client" in the upper-righthand corner.</li>
-        <li>
-          On this page, enter the following information into the form:
-          <ul>
-            <li><strong>Application Name</strong>: The name of your application. Choose something that is easy to remember and relates to your website in a meaningful way.</li>
-            <li><strong>Description</strong>: Describe your application and its functionality.</li>
-            <li><strong>Website</strong>: The URL of this website ( <?php echo get_site_url(); ?> )</li>
-            <li><strong>OAuth redirect_uri</strong>: Use the following URL: <br /><input type="text" id="url_text" disabled value="<?php echo  get_site_url(); ?>/wp-admin/admin-ajax.php?action=register_instagram"><button id="d_clip_button" data-clipboard-target="url_text" class="mini-button" value="Copy to Clipboard"><i class="fa fa-clipboard"></i> Copy</button></li>
-          </ul>
-        </li>
-        <li>Click the "Register" button. On the resulting page, take note of the Client ID and Client Secret values for the next step.</li>
-      </ol>
-	  </div>
-	  <script>
-	ZeroClipboard.setDefaults( { moviePath: '<?php echo  str_replace('admin/views', 'admin/assets/js/', plugins_url( NULL , __FILE__ )); ?>ZeroClipboard.swf' } );
-</script>
-	</section>
-	
-	<section id="step02">
-	  <div class="section_title">
-      Step 02: Copy Your Client ID and Client Secret
-    </div>
-    <div class="section_content <?php if($set == 1){ echo "active"; } ?>">
-  	  <form action="options.php" method="post">
-        <?php settings_fields('simple-instagram_options'); ?>
-        <?php do_settings_sections('simple-instagram'); ?>
-        <input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
-      </form>
-    </div>
-	</section>
-	
-	<section id="step03">
-	  <div class="section_title">
-      Step 03: Authorize Your Instagram Application
-    </div>
-    <div class="section_content <?php if($set == 1){ echo "active"; } ?>">
-     <?php if($set == 1){ ?>
-	   <iframe width="100%" src="<?php echo  get_site_url(); ?>/wp-admin/admin-ajax.php?action=register_instagram"></iframe>
-	   <?php }else{ ?>
-	     Please follow steps 1 and 2 first.
-	   <?php } ?>
-	  </div>
-	</section>
-	
-	<?php if(get_option('si_oauth') && get_option('si_oauth') != ''){ ?>
-		<section id="search">
-			<div class="section_title">
-				Lookup User ID
-			</div>
-			<div class="section_content active">
-			    <p>In order to display the feed of another user, you'll need to know their user ID. Use the form below to search for a user by their username.</p>
-			    <form>
-			    	<label>Username:</label>
-			    	<input type="text" name="user_name">
-			    	<button class="search_user" value="Search">Search!</button>
-			    	<div id="search_results"></div>
-			    </form>
-			</div>
-		</section>
-	<?php } ?>
-
+    <h2><?php _e( 'Simple Instagram Settings', 'simple-instagram' ); ?></h2>
+    <section id="authorize">
+        <div class="section_title">
+            <?php _e( 'Authorize Your Instagram Account', 'simple-instagram' ); ?>
+        </div>
+        <div class="section_content active">
+            <?php if ( is_null( $token ) ): ?>
+                <h3><?php _e( 'Account not yet Authorized', 'simple-instagram' ); ?></h3>
+                <p><?php _e( 'Before you can display your Instagram feeds, you will need to authorize your Instagram account.', 'simple-instagram' ); ?></p>
+                <p><?php _e( 'Use the button below to begin the Authorization process. You will be redirected to Instagram to sign in and authorize this plugin. Once you authorize the plugin, you will be redirected to this page.', 'simple-instagram' ); ?></p>
+                <a href="https://api.instagram.com/oauth/authorize/?client_id=f565b2166806431eb8ffbd19d7ea257a&response_type=code&redirect_uri=http://plugins.westwerk.com/si/authenticate_user.php?return_uri=<?php echo esc_url( admin_url( 'options-general.php?page=simple-instagram' ) ); ?>" class="button button-primary"><?php _e( 'Authorize with Instagram', 'simple-instagram' ); ?></a>
+            <?php else : ?>
+                <h3><?php _e( 'Your account has successfully been authorized to use Simple Instagram!', 'simple-instagram' ); ?></h3>
+                <p><?php _e( 'Feeds not displaying? There might be a problem with your current Authorization. Use the button below to try re-authorizing with Instagram.', 'simple-instagram' ); ?></p>
+                <p><a href="https://api.instagram.com/oauth/authorize/?client_id=f565b2166806431eb8ffbd19d7ea257a&response_type=code&redirect_uri=http://plugins.westwerk.com/si/authenticate_user.php?return_uri=<?php echo esc_url( admin_url( 'options-general.php?page=simple-instagram' ) ); ?>" class="button button-secondary"><?php _e( 'Re-Authorize with Instagram', 'simple-instagram' ); ?></a></p>
+            <?php endif; ?>
+        </div>
+    </section>
+    <?php if ( ! is_null( $token ) ): ?>
+        <section id="search">
+            <div class="section_title">
+                <?php _e( 'Lookup User ID', 'simple-instagram' ); ?>
+            </div>
+            <div class="section_content active">
+                <p><?php _e( "In order to display the feed of another user, you'll need to know their user ID. Use the form below to search for a user by their username.", 'simple-instagram' ); ?></p>
+                <form>
+                    <label><?php _e( 'Username:', 'simple-instagram' ); ?></label>
+                    <input type="text" name="user_name">
+                    <button class="search_user button button-secondary" value="Search"><?php _e( 'Search!', 'simple-instagram' ); ?></button>
+                    <div id="search_results"></div>
+                </form>
+            </div>
+        </section>
+    <?php endif; ?>
 </div>
-
--- simple-instagram.php
+++ simple-instagram.php
@@ -2,8 +2,7 @@
 /**
  * simple-instagram
  *
- * A plugin to allow users to include InstaGram feeds, media, and information. 
- * 
+ * A plugin to allow users to include Instagram feeds, media, and information.
  *
  * @package   Simple Instagram
  * @author    Aaron Speer <adspeer@gmail.com>
@@ -14,7 +13,7 @@
  * @wordpress-plugin
  * Plugin Name:       Simple Instagram
  * Description:       A plugin to allow users to include InstaGram feeds, media, and information.
- * Version:           1.2
+ * Version:           2.0
  * Author:            Aaron Speer
  * Author URI:        aaronspeer.com
  * Text Domain:       simple-instagram
@@ -26,183 +25,25 @@
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
-	die;
+    die;
 }
 
-/*----------------------------------------------------------------------------*
- * Public-Facing Functionality
- *----------------------------------------------------------------------------*/
+define( 'SI_PLUGIN_DIR', dirname( __FILE__ ) );
+define( 'SI_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-/*
- *
+include_once SI_PLUGIN_DIR . '/inc/admin.php';
+include_once SI_PLUGIN_DIR . '/inc/scripts.php';
+include_once SI_PLUGIN_DIR . '/inc/widgets.php';
+include_once SI_PLUGIN_DIR . '/inc/shortcodes.php';
+include_once SI_PLUGIN_DIR . '/inc/ajax.php';
+
+add_action( 'plugins_loaded', 'si_init' );
+
+/**
+ * SI Init - Initialize the main Admin Class
  */
-require_once( plugin_dir_path( __FILE__ ) . 'public/simple-instagram.php' );
+function si_init() {
 
-//Get Shortcodes file
-require_once( plugin_dir_path( __FILE__ ) . 'includes/simple-instagram-shortcodes.php' );
-
-//Get Widget File
-require_once( plugin_dir_path( __FILE__ ) . 'includes/simple-instagram-widgets.php' );
-
-
-/*
- * Register hooks that are fired when the plugin is activated or deactivated.
- * When the plugin is deleted, the uninstall.php file is loaded.
- *
- */
-register_activation_hook( __FILE__, array( 'simpleInstagram', 'activate' ) );
-register_deactivation_hook( __FILE__, array( 'simpleInstagram', 'deactivate' ) );
-
-/*
- */
-add_action( 'plugins_loaded', array( 'simpleInstagram', 'get_instance' ) );
-
-// Add register iframe
-add_action('wp_ajax_register_instagram', 'register_instagram');
-add_action('wp_ajax_search_users', 'search_users');
-
-function register_instagram(){
-  	$options_check = get_option('si_options');
-	$config = array(
-	            'apiKey'      => $options_check['instagram_app_id'],
-	            'apiSecret'   => $options_check['instagram_app_secret'],
-	            'apiCallback' => site_url() . '/wp-admin/admin-ajax.php?action=register_instagram'
-	          );
-	
-	if(isset($_GET['code'])){
-	  $options_check = get_option('si_options');
-	  $instagram = new Instagram($config);
-	  $token = $instagram->getOAuthToken($_GET['code']);
-	  if(isset($token->access_token)){
-	  	update_option( 'si_oauth', $token->access_token );
-	  }else{
-	  	update_option( 'si_oauth', 'error');
-	  }
-	}
-	
-	
-	if(strlen($options_check['instagram_app_id']) > 0 && strlen($options_check['instagram_app_secret']) > 0){
-	  $set = 1;
-	}else{
-	  $set = 0;
-	}
-	
-	$auth_check = get_option('si_oauth');
-	if(strlen($auth_check) > 0){
-	  $auth = 1;
-	}else{
-	  $auth = 0;
-	}?>
-	
-	<head>
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-		<link href='<?php echo plugins_url( 'admin/assets/css/iframe.css' , __FILE__ ); ?>' rel='stylesheet' type='text/css'>
-		<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-	</head>
-	
-	<?php if($auth == 0){
-	  $instagram = new Instagram($config);
-	  // Display the login button
-	  $loginUrl = $instagram->getLoginUrl();
-	?> 
-	
-	<div class="instagram">
-		<span><i class="fa fa-instagram"></i></span><a href="<?php echo $loginUrl; ?>" target="_blank">Login with Instagram</a>
-	</div>
-	<script>
-		setTimeout(function(){ location.reload(); },5000);
-	</script>
-	<?php }else{
-		if($auth_check == 'error'){
-			//Error with auth
-			$instagram = new Instagram($config);
-			// Display the login button
-			$loginUrl = $instagram->getLoginUrl();
-			?>
-			<p>Whoops! It looks like there's a problem with your App credentials. Please check your entries in Step 02 and then use the button below to authorize the app once again.</p>
-			<div class="instagram">
-				<span><i class="fa fa-instagram"></i></span><a href="<?php echo $loginUrl; ?>" target="_blank">Login with Instagram</a>
-			</div>
-			<script>
-				setTimeout(function(){ location.reload(); },5000);
-			</script>
-		<?php }else{
-		  //We have auth credentials, check to make sure they haven't expired
-		  $instagram = new Instagram($config);
-		  $instagram->setAccessToken($auth_check);
-		  $user = $instagram->getUser();
-		  if(isset($user->data->username)){?>
-		    <h2>Success!</h2>
-		    <p>Alright! You're all set up and ready to go!</p>
-		  <?php }else{
-		  	//Auth token has expired. Show login button instead. 
-		    $instagram = new Instagram($config);
-		  	// Display the login button
-		  	$loginUrl = $instagram->getLoginUrl();
-		  ?>
-		  <p>Whoops! It looks like your authorization has expired. Please use the button below to authorize the app once again.</p>
-		  <div class="instagram">
-				<span><i class="fa fa-instagram"></i></span><a href="<?php echo $loginUrl; ?>" target="_blank">Login with Instagram</a>
-			</div>
-			<script>
-				setTimeout(function(){ location.reload(); },5000);
-			</script>
-		  <?php } ?>
-		<?php }
-		}
-	exit;
-  }
-
-function search_users(){
-  	$options = get_option('si_options');
-    $auth = get_option('si_oauth');
-	$user = $_POST['user'];
-    $config = array(
-          'apiKey'      => $options['instagram_app_id'],
-          'apiSecret'   => $options['instagram_app_secret'],
-          'apiCallback' => site_url() . '/wp-admin/admin-ajax.php?action=register_instagram'
-        );
-   
-    $instagram = new Instagram($config);
-    $instagram->setAccessToken($auth);
-	
-	$feed = $instagram->searchUser($user, 20);
-	
-	foreach($feed->data as $result){ ?>
-		<div class="si_sr">
-			<div class="si_sr_user_image">
-				<img src="<?php echo $result->profile_picture; ?>">
-			</div>
-			<div class="si_sr_user_info">
-				<div class="si_sr_user_name">
-					<strong>username: </strong><?php echo $result->username; ?>
-				</div>
-				<div class="si_sr_user_id">
-					<strong>user ID: </strong><?php echo $result->id; ?>
-				</div>
-			</div>
-		</div>
-	<?php }
-	exit;
-  }
-/*----------------------------------------------------------------------------*
- * Dashboard and Administrative Functionality
- *----------------------------------------------------------------------------*/
-
-/*
- *
- * If you want to include Ajax within the dashboard, change the following
- * conditional to:
- *
- * if ( is_admin() ) {
- *   ...
- * }
- *
- * The code below is intended to to give the lightest footprint possible.
- */
-if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
-
-	require_once( plugin_dir_path( __FILE__ ) . 'admin/simple-instagram-admin.php' );
-	add_action( 'plugins_loaded', array( 'simpleInstagram_admin', 'get_instance' ) );
+    $si_admin = SI_Admin::get_instance();
 
 }
